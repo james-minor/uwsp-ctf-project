dc80f549cae26fb5036de4534cc6799b4dd6f84b
James Minor <james.minor.business@gmail.com>
2023-09-13 11:43:28 -0500

    Express can now access the Postgres db.

--- express/src/index.ts
+++ express/src/index.ts
@@ -1,10 +1,41 @@
 import express, {Express, NextFunction, Request, Response, Router} from 'express';
 import 'dotenv/config';
 import Logger from './logging/Logger';
+import { Pool } from 'pg';
 
 const port: number = 8000;  // Always use port 8000, as the port is remapped via Compose using our env var EXPRESS_PORT.
 const app: Express = express();
 const router: Router = express.Router();
+
+Logger.outputToConsole = process.env.ENVIRONMENT_TYPE === 'development';
+
+/* Setting up the connection pool to the Postgres service.
+ */
+if (process.env.POSTGRES_USERNAME === undefined || process.env.POSTGRES_PASSWORD === undefined)
+{
+	Logger.error('Cannot access Postgres service. Stopping express service.');
+	Logger.error('Ensure POSTGRES_USERNAME and POSTGRES_PASSWORD environment variables are set.');
+}
+
+const pool: Pool = new Pool({
+	host: 'postgres',
+	port: 5432,
+	user: process.env.POSTGRES_USERNAME,
+	password: process.env.POSTGRES_PASSWORD,
+	max: 50,
+});
+
+/* Validating that the pool is able to successfully connect to the Postgres service.
+ */
+pool.connect().then(() =>
+{
+	Logger.info('Successfully connected to the Postgres service!');
+}).catch(error =>
+{
+	Logger.error('Could not connect to the Postgres service, stopping express service.');
+	Logger.error(error.message);
+	process.exit();
+});
 
 /* Logging middleware.
  */
@@ -35,11 +66,5 @@ app.use('/api/v1/', router);
  */
 app.listen(port, () =>
 {
-	Logger.outputToConsole = process.env.ENVIRONMENT_TYPE === 'development';
 	Logger.info(`Express API listening on port ${port}...`);
-});
-
-router.get('/', (req: Request, res: Response) =>
-{
-	res.status(400).json({body: 'Hello world!'});
 });
