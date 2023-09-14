36d165bae4cd6670f7484d74988c72dfe8dfbff6
James Minor <james.minor.business@gmail.com>
2023-09-13 21:19:30 -0500

    No longer use POSTGRES_DB or POSTGRES_USERNAME, not required.

--- express/src/index.ts
+++ express/src/index.ts
@@ -4,6 +4,7 @@ import Logger from './logging/Logger';
 import { Pool } from 'pg';
 
 const port: number = 8000;  // Always use port 8000, as the port is remapped via Compose using our env var EXPRESS_PORT.
+const maxClients: number = process.env.POSTGRES_MAX_CLIENTS === undefined ? 50 : parseInt(process.env.POSTGRES_MAX_CLIENTS);
 const app: Express = express();
 const router: Router = express.Router();
 
@@ -31,18 +32,18 @@ Logger.outputToConsole = environmentType === 'development';
 
 /* Setting up the connection pool to the Postgres service.
  */
-if (process.env.POSTGRES_USERNAME === undefined || process.env.POSTGRES_PASSWORD === undefined)
+if (process.env.POSTGRES_PASSWORD === undefined)
 {
 	Logger.error('Cannot access Postgres service. Stopping express service.');
-	Logger.error('Ensure POSTGRES_USERNAME and POSTGRES_PASSWORD environment variables are set.');
+	Logger.error('Ensure the POSTGRES_PASSWORD environment variable is set.');
 }
 
 const pool: Pool = new Pool({
 	host: 'postgres',
 	port: 5432,
-	user: process.env.POSTGRES_USERNAME,
+	user: 'postgres',
 	password: process.env.POSTGRES_PASSWORD,
-	max: 50,
+	max: maxClients,
 });
 
 /* Validating that the pool is able to successfully connect to the Postgres service.
