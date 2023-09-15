ee62adda59faa14a9718eb19e7474a0583da5282
James Minor <james.minor.business@gmail.com>
2023-09-15 07:50:59 -0500

    Split Express server into class file and runner

---
+++ express/src/Server.ts
@@ -0,0 +1,94 @@
+import express, { Express, NextFunction, Request, Response, Router } from 'express';
+import { Pool } from 'pg';
+import ILogger from './logging/ILogger';
+import fs from 'fs';
+import path from 'path';
+
+export default class Server
+{
+	public app: Express;
+	public router: Router;
+
+	public pool: Pool;
+	public logger: ILogger;
+
+	public port: number;
+	public environment: 'development' | 'production';
+
+	constructor(
+		pool: Pool,
+		logger: ILogger,
+		port: number = 8000,
+		environment?: 'development' | 'production'
+	)
+	{
+		this.pool = pool;
+		this.logger = logger;
+
+		this.app = express();
+		this.router = express.Router();
+		this.port = port;
+
+		this.environment = environment === undefined ? 'production' : environment;
+	}
+
+	public start()
+	{
+		this.setupMiddleware();
+		this.linkRoutes();
+
+		this.app.listen(this.port, () =>
+		{
+			this.logger.info(`Express API listening on port ${this.port}...`);
+		});
+	}
+
+	protected setupMiddleware()
+	{
+		/* Setting up the logging middleware to log all API requests.
+		 */
+		this.app.use((req: Request, res: Response, next: NextFunction) =>
+		{
+			this.logger.info(`${req.method} ${req.url} ${req.protocol.toUpperCase()}/${req.httpVersion}`);
+			next();
+		});
+
+		/* Application-level middleware that will prevent API access unless the remote host is our Astro service
+		 * container. (Note: this only happens in PRODUCTION mode, in DEVELOPMENT mode any remote host can access
+		 * the API).
+		 */
+		if (this.environment === 'production')
+		{
+			this.app.use((req: Request, res: Response, next: NextFunction) =>
+			{
+				if (req.ip === 'https://astro:4321' || req.ip === 'http://astro:4321')
+				{
+					next();
+				}
+
+				res.status(403).json({error: 'Attempted access from invalid remote host.'});
+			});
+		}
+
+		/* Prefixing '/api/v1/' to all internal routing.
+		 */
+		this.app.use('/api/v1/', this.router);
+	}
+
+	protected linkRoutes()
+	{
+		/* Recursively getting the routes from the 'routes' directory. We add all the route files at run-time, allowing
+		 * for the file system to define the route path.
+		 */
+		let routes = fs.readdirSync(path.join(__dirname, 'routes'), { recursive: true });
+		routes.forEach(route =>
+		{
+			if (typeof route === 'string' && route.includes('.js'))
+			{
+				let endpoint = '/' + route.replace('.js', '');
+
+				require('./routes' + endpoint)(endpoint, this);
+			}
+		});
+	}
+}
--- express/src/index.ts
+++ express/src/index.ts
@@ -1,12 +1,11 @@
-import express, {Express, NextFunction, Request, Response, Router} from 'express';
 import 'dotenv/config';
-import Logger from './logging/Logger';
 import { Pool } from 'pg';
+import Server from './Server';
+import Logger from './logging/Logger';
 
-const port: number = 8000;  // Always use port 8000, as the port is remapped via Compose using our env var EXPRESS_PORT.
+const port: number = 8000;
+const logger = new Logger();
 const maxClients: number = process.env.POSTGRES_MAX_CLIENTS === undefined ? 50 : parseInt(process.env.POSTGRES_MAX_CLIENTS);
-const app: Express = express();
-const router: Router = express.Router();
 
 /* The current environment type. Defaults to 'production', set to 'development' to output logging to the console,
  * and to allow for connection from any remote host.
@@ -15,27 +14,29 @@ const environmentType: 'development' | 'production' = (() =>
 {
 	if (process.env.ENVIRONMENT_TYPE == undefined)
 	{
-		Logger.warn('ENVIRONMENT_TYPE environment variable not set. Defaulting to "production"...');
+		logger.outputToConsole = false;
+		logger.warn('ENVIRONMENT_TYPE environment variable not set. Defaulting to "production"...');
 		return 'production';
 	}
 
 	switch(process.env.ENVIRONMENT_TYPE.toLowerCase())
 	{
 		case 'development':
+			logger.outputToConsole = true;
 			return 'development';
 		default:
+			logger.outputToConsole = false;
 			return 'production';
 	}
 })();
-
-Logger.outputToConsole = environmentType === 'development';
 
 /* Setting up the connection pool to the Postgres service.
  */
 if (process.env.POSTGRES_PASSWORD === undefined)
 {
-	Logger.error('Cannot access Postgres service. Stopping express service.');
-	Logger.error('Ensure the POSTGRES_PASSWORD environment variable is set.');
+	logger.error('Cannot access Postgres service. Stopping express service.');
+	logger.error('Ensure the POSTGRES_PASSWORD environment variable is set.');
+	process.exit();
 }
 
 const pool: Pool = new Pool({
@@ -50,61 +51,15 @@ const pool: Pool = new Pool({
  */
 pool.connect().then(() =>
 {
-	Logger.info('Successfully connected to the Postgres service!');
+	logger.info('Successfully connected to the Postgres service!');
 }).catch(error =>
 {
-	Logger.error('Could not connect to the Postgres service, stopping express service.');
-	Logger.error(error.message);
+	logger.error('Could not connect to the Postgres service, stopping express service.');
+	logger.error(error.message);
 	process.exit();
 });
 
-/* Logging middleware.
+/* Starting the API Server.
  */
-app.use((req: Request, res: Response, next: NextFunction) =>
-{
-	Logger.info(`${req.method} ${req.url} ${req.protocol.toUpperCase()}/${req.httpVersion}`);
-	next();
-});
-
-/* Application-level middleware that will prevent API access unless the remote host is our Astro service
- * container. (Note: this only happens in PRODUCTION mode, in DEVELOPMENT mode any remote host can access
- * the API).
- */
-if (environmentType === 'production')
-{
-	app.use((req: Request, res: Response, next: NextFunction) =>
-	{
-		if (req.ip === 'https://astro:4321' || req.ip === 'http://astro:4321')
-		{
-			next();
-		}
-
-		res.status(403).json({error: 'Attempted access from invalid remote host.'});
-	});
-}
-
-/* Initializing the Express router.
- */
-app.use('/api/v1/', router);
-
-/* Starting the express server.
- */
-app.listen(port, () =>
-{
-	Logger.info(`Express API listening on port ${port}...`);
-});
-
-router.get('/admin/count', (req: Request, res: Response) =>
-{
-	pool.query(`
-		SELECT COUNT(*) 
-		FROM user_accounts
-		WHERE role='ADMIN'
-		`
-	).then(result =>
-	{
-		res.status(200).json({
-			count: parseInt(result.rows[0].count)
-		});
-	});
-});
+const server = new Server(pool, logger, port, environmentType);
+server.start();
