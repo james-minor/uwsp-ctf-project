390a8f6d9950a5db250c2adfa5062f75ab6efa0b
James Minor <james.minor.business@gmail.com>
2023-09-19 16:56:46 -0500

    Added IF NOT EXISTS safeguard for table initialization.

--- postgres/init.sql
+++ postgres/init.sql
@@ -3,7 +3,7 @@ CREATE TYPE user_role AS ENUM (
     'ADMIN'
 );
 
-CREATE TABLE user_accounts (
+CREATE TABLE IF NOT EXISTS user_accounts (
     user_id         SERIAL PRIMARY KEY,
     username        varchar(30) NOT NULL,
     password_hash   char(60) NOT NULL,
@@ -11,17 +11,17 @@ CREATE TABLE user_accounts (
     role            user_role NOT NULL DEFAULT 'USER'
 );
 
-CREATE TABLE teams (
+CREATE TABLE IF NOT EXISTS teams (
     team_id         SERIAL PRIMARY KEY,
     name            varchar(30) NOT NULL
 );
 
-CREATE TABLE categories (
+CREATE TABLE IF NOT EXISTS categories (
     category_id     SERIAL PRIMARY KEY,
     title           varchar(15) NOT NULL
 );
 
-CREATE TABLE challenges (
+CREATE TABLE IF NOT EXISTS challenges (
     challenge_id    SERIAL PRIMARY KEY,
     category_id     SERIAL NOT NULL REFERENCES categories(category_id),
     value           int NOT NULL DEFAULT 0,
@@ -30,14 +30,14 @@ CREATE TABLE challenges (
     attachments     varchar(100)[3]
 );
 
-CREATE TABLE announcements (
+CREATE TABLE IF NOT EXISTS announcements (
     announcement_id     SERIAL PRIMARY KEY,
     creation_date       timestamp NOT NULL,
     author_user_id      SERIAL NOT NULL REFERENCES user_accounts(user_id),
     body                varchar(1000) NOT NULL
 );
 
-CREATE TABLE scores (
+CREATE TABLE IF NOT EXISTS scores (
     score_id            SERIAL PRIMARY KEY,
     team_id             SERIAL NOT NULL REFERENCES teams(team_id),
     challenge_id        SERIAL NOT NULL REFERENCES challenges(challenge_id),
