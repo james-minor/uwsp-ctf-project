77a3de8429bccba61b49d9e2bcffac5e42a9064a
James Minor <james.minor.business@gmail.com>
2023-09-21 08:58:16 -0500

    Updated /admin/count implementation.

--- express/src/routes/admin/count.ts
+++ express/src/routes/admin/count.ts
@@ -1,15 +1,20 @@
 import { Request, Response } from 'express';
+import { Role } from '@prisma/client';
+
 import Server from '../../Server';
 
 module.exports = function(path: string, server: Server)
 {
-	server.router.get(path, (req: Request, res: Response) =>
+	server.router.get(path, async (req: Request, res: Response) =>
 	{
-		server.pool.query(`SELECT COUNT(*) FROM user_accounts WHERE role='ADMIN'`).then(result =>
-		{
-			res.status(200).json({
-				count: parseInt(result.rows[0].count)
-			});
+		const result = await server.client.user.count({
+			where: {
+				role: Role.ADMIN
+			}
+		});
+
+		res.status(200).json({
+			count: result
 		});
 	});
 }
