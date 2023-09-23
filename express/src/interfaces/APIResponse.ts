70634ef76f1d32acc6c155ee40083418217e3492
James Minor <james.minor.business@gmail.com>
2023-09-23 09:19:30 -0500

    Added the APIResponse interface.

---
+++ express/src/interfaces/APIResponse.ts
@@ -0,0 +1,12 @@
+export interface APIResponseError
+{
+	key: string;
+	message: string;
+}
+
+export interface APIResponse
+{
+	success: boolean;
+	data: object;
+	errors?: APIResponseError[];
+}
--- express/src/routes/admin/count.ts
+++ express/src/routes/admin/count.ts
@@ -2,19 +2,23 @@ import { Request, Response } from 'express';
 import { Role } from '@prisma/client';
 
 import Server from '../../Server';
+import { APIResponse } from '../../interfaces/APIResponse';
 
 module.exports = function(path: string, server: Server)
 {
-	server.router.get(path, async (req: Request, res: Response) =>
+	server.router.get(path, async (req: Request, res: Response<APIResponse>) =>
 	{
-		const result = await server.client.user.count({
+		const result = await server.prisma.user.count({
 			where: {
 				role: Role.ADMIN
 			}
 		});
 
 		res.status(200).json({
-			count: result
+			success: true,
+			data: {
+				count: result
+			}
 		});
 	});
 }
