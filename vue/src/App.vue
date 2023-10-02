54b3d6bdcddce06bb10dccd23acfd03e04956e54
James Minor <james.minor.business@gmail.com>
2023-10-01 22:41:47 -0500

    Vue app now provides an API URL to all child components.

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
+	import { provide } from 'vue';
 	import NavigationBar from './components/NavigationBar.vue';
+
+	if (import.meta.env.VITE_API_URL === undefined)
+	{
+		provide('apiBaseURL', 'http://127.0.0.1:80/api')
+	}
+	else
+	{
+		provide('apiBaseURL', import.meta.env.VITE_API_URL);
+	}
+
 </script>
 
 <template>
