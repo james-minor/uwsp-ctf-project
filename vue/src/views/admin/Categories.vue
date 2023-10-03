b7cb93496460fc6714c38b152d007e276c1a9e18
James Minor <james.minor.business@gmail.com>
2023-10-03 09:34:09 -0500

    Scaffolded in admin sub-pages.

---
+++ vue/src/views/admin/Categories.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+import NavigationBar from '@/components/NavigationBar.vue';
+</script>
+
+<template>
+	<navigation-bar/>
+	Categories
+</template>
+
+<style scoped>
+
+</style>
