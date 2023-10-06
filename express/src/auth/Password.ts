a146c9573ab3cbb3f5b0dec54781f4e2d80afa9b
James Minor <james.minor.business@gmail.com>
2023-10-06 18:39:32 -0500

    Added Password class, along with integration tests

---
+++ express/src/auth/Password.ts
@@ -0,0 +1,41 @@
+import bcrypt from 'bcryptjs';
+import Client from '../Client';
+
+export default class Password
+{
+	/**
+	 * Generates a password hash from a passed plain-text password.
+	 * @param password The plain text password to hash.
+	 */
+	static async create(password: string)
+	{
+		return new Promise((resolve, reject) =>
+		{
+			bcrypt.hash(password, 10, async (err, hash) =>
+			{
+				err ? reject(err) : resolve(hash);
+			});
+		});
+	}
+
+	/**
+	 * Validates a username and password are valid.
+	 * @param username The plain text username to validate.
+	 * @param password The plain text password to validate.
+	 */
+	static async validate(username: string, password: string)
+	{
+		const user = await Client.user.findUnique({
+			where: {
+				username: username
+			}
+		});
+
+		if(!user)
+		{
+			return false;
+		}
+
+		return await bcrypt.compare(password, user.passwordHash)
+	}
+}
