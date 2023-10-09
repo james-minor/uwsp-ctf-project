dcf73108ccc66b91ab39bb638d38128c5c17472d
James Minor <james.minor.business@gmail.com>
2023-10-09 09:05:26 -0500

    Added Team and Announcement admin views

---
+++ vue/src/views/admin/Announcements.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+import NavigationBar from '@/components/NavigationBar.vue';
+</script>
+
+<template>
+	<navigation-bar/>
+	Announcements
+</template>
+
+<style scoped>
+
+</style>
