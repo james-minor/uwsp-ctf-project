f2875d8549a6802da2e1425c30ca9d7bbd6fa20b
James Minor <james.minor.business@gmail.com>
2023-10-09 09:02:29 -0500

    Implemented NavigationBar in Scoreboard view

--- vue/src/views/Scoreboard.vue
+++ vue/src/views/Scoreboard.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">   
     
+import NavigationBar from '@/components/NavigationBar.vue';
 </script>
 
 <template>
+	<navigation-bar/>
     <h1>Scoreboard page</h1>
 </template>
 
