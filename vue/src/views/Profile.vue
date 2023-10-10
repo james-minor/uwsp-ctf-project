11db7e58a9eaa8aaa764fad08b15e6b9e79adef2
James Minor <james.minor.business@gmail.com>
2023-10-09 22:43:09 -0500

    Added Profile view.

---
+++ vue/src/views/Profile.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+import NavigationBar from '@/components/NavigationBar.vue';
+</script>
+
+<template>
+	<navigation-bar/>
+	<h1>Profile page</h1>
+</template>
+
+<style scoped>
+
+</style>
