e2c14fd3bdb78a9fcf63f9d3d64a57e041b7e428
James Minor <james.minor.business@gmail.com>
2023-10-10 15:10:17 -0500

    App now programmatically shows / hides navigation based on route meta fields

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,18 +1,42 @@
 <script setup lang="ts">
-	import { provide } from 'vue';
+	import { provide, ref } from 'vue';
 	import { apiBaseURL } from '@/APIBaseURL';
+	import NavigationBar from '@/components/NavigationBar.vue';
+	import AdminSidebar from '@/components/AdminSidebar.vue';
+	import router from '@/router/router';
 
-	provide('apiBaseURL', apiBaseURL);
+	const navbarVisible = ref<boolean>(true);
+	const sidebarVisible = ref<boolean>(false);
+
+	router.beforeEach((to, from) =>
+	{
+		navbarVisible.value = to.meta.hideMainNav ? !to.meta.hideMainNav : true;
+		sidebarVisible.value = to.meta.showAdminNav ? to.meta.showAdminNav : false;
+	})
+
+	provide<string>('apiBaseURL', apiBaseURL);
 </script>
 
 <template>
-	<main>
+	<navigation-bar v-if="navbarVisible"></navigation-bar>
+	<admin-sidebar v-if="sidebarVisible"></admin-sidebar>
+
+	<main :class="{ 'sidebarPresent' : sidebarVisible, 'navbarPresent' : navbarVisible }">
 		<router-view></router-view>
 	</main>
 </template>
 
 <style scoped>
 	main {
+		display: flex;
 		min-height: 100svh;
+	}
+
+	.navbarPresent {
+		padding-top: var(--len-navbar-height);
+	}
+
+	.sidebarPresent {
+		padding-left: var(--len-sidebar-width);
 	}
 </style>
