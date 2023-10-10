3884b501f2829252a3b8fe7d871bfaf256d3d0b0
James Minor <james.minor.business@gmail.com>
2023-10-10 15:08:10 -0500

    Added the AdminSidebar component

---
+++ vue/src/components/AdminSidebar.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<aside>
+		<nav>
+			<router-link to="/admin">Dashboard</router-link>
+			<router-link to="/admin/announcements">Announcements</router-link>
+			<router-link to="/admin/categories">Categories</router-link>
+			<router-link to="/admin/teams">Teams</router-link>
+			<router-link to="/admin/tools">Tools</router-link>
+			<router-link to="/admin/users">Users</router-link>
+			<router-link to="/admin/waves">Waves</router-link>
+		</nav>
+	</aside>
+</template>
+
+<style scoped>
+	aside {
+		position: fixed;
+		top: var(--len-navbar-height);
+		left: 0;
+
+		width: var(--len-sidebar-width);
+		height: calc(100% - var(--len-navbar-height));
+
+		background-color: var(--col-body-dark-100);
+		border-right-color: var(--col-body-dark-200);
+		border-right-width: 2px;
+		border-right-style: solid;
+	}
+
+	nav {
+		height: 100%;
+
+		display: flex;
+		flex-direction: column;
+		align-items: stretch;
+	}
+
+	a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		color: white;
+
+		flex: 1;
+
+		text-decoration: none;
+
+		outline: none;
+		transition: background-color 0.2s;
+	}
+
+	a:focus-visible,
+	a:hover {
+		background-color: var(--col-body-dark-200);
+	}
+</style>
