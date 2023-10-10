309cd119833c97c0413b2e0b3df066c2e7495e39
James Minor <james.minor.business@gmail.com>
2023-10-09 22:44:18 -0500

    Added authenticated routes to router.

--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -1,14 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import type { RouteRecordRaw } from 'vue-router';
+import type { RouteRecordRaw, RouteMeta } from 'vue-router';
 
-import Home from '../views/Home.vue';
-import Rules from '../views/Rules.vue';
-import Challenges from '../views/Challenges.vue';
-import Scoreboard from '../views/Scoreboard.vue';
-import Login from '../views/Login.vue';
-import Register from '../views/Register.vue';
+import Home from '@/views/Home.vue';
+import Rules from '@/views/Rules.vue';
+import Challenges from '@/views/Challenges.vue';
+import Scoreboard from '@/views/Scoreboard.vue';
+import Login from '@/views/Login.vue';
+import Register from '@/views/Register.vue';
 
-const routes: RouteRecordRaw[] = 
+import Dashboard from '@/views/admin/Dashboard.vue';
+import Categories from '@/views/admin/Categories.vue';
+import Tools from '@/views/admin/Tools.vue';
+import Users from '@/views/admin/Users.vue';
+import Waves from '@/views/admin/Waves.vue';
+import Profile from '@/views/Profile.vue';
+
+declare module 'vue-router'
+{
+    interface RouteMeta
+    {
+        requiresAuth?: boolean,     // User must be authenticated to access the route.
+        requiresAdmin?: boolean,    // User must be authenticated AND have admin privileges to access the route.
+    }
+}
+
+const routes: RouteRecordRaw[] =
 [
     { path: '/', component: Home },
     { path: '/rules', component: Rules },
@@ -16,6 +32,13 @@ const routes: RouteRecordRaw[] =
     { path: '/scoreboard', component: Scoreboard },
     { path: '/login', component: Login },
     { path: '/register', component: Register },
+    { path: '/profile', component: Profile, meta: { requiresAuth: true } },
+    { path: '/admin', component: Dashboard, meta: { requiresAdmin: true } },
+    { path: '/admin/categories', component: Categories, meta: { requiresAdmin: true } },
+    { path: '/admin/challenges', component: Challenges, meta: { requiresAdmin: true } },
+    { path: '/admin/tools', component: Tools, meta: { requiresAdmin: true } },
+    { path: '/admin/users', component: Users, meta: { requiresAdmin: true } },
+    { path: '/admin/waves', component: Waves, meta: { requiresAdmin: true } },
 ];
 
 const router = createRouter({
