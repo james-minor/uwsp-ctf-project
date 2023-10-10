444aef3070d60057d66942b616ee43159bc3685b
James Minor <james.minor.business@gmail.com>
2023-10-09 23:03:41 -0500

    Swapped API urls in ternary operation

--- vue/src/APIBaseURL.ts
+++ vue/src/APIBaseURL.ts
@@ -1,1 +1,1 @@
-export const apiBaseURL = import.meta.env.VITE_API_URL ? 'http://127.0.0.1/api' : import.meta.env.VITE_API_URL;
+export const apiBaseURL = import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : 'http://127.0.0.1:80/api';
