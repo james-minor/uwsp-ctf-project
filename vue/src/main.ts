0c60f52dff162a7cd33b7d378c88e581029a7ff2
James Minor <james.minor.business@gmail.com>
2023-10-17 21:47:01 -0500

    Implemented Pinia store

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import router from './router/router';
 
 import App from './App.vue';
 
 import './assets/main.css';
 
+const pinia = createPinia();
 const app = createApp(App);
+
+app.use(pinia);
 app.use(router);
 app.mount('#app');
