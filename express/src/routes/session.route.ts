16c950e444a1698cb7ff70ff7da79b3d9978064b
James Minor <james.minor.business@gmail.com>
2023-10-18 10:22:08 -0500

    Added userGuard to DELETE /session

--- express/src/routes/session.route.ts
+++ express/src/routes/session.route.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import * as sessionController from '../controllers/session.controller';
+import userGuard from '../auth/user.guard';
 
 module.exports = function (router: Router)
 {
 	router.post('/session', sessionController.login);
 
-	router.delete('/session', sessionController.logout);
+	router.delete('/session', [userGuard, sessionController.logout]);
 }
