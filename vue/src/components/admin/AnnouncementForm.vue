ac542a014ba3f0d666310586475849b3da2bee08
James Minor <james.minor.business@gmail.com>
2023-10-19 17:31:58 -0500

    Implemented Announcements admin page functionality

---
+++ vue/src/components/admin/AnnouncementForm.vue
@@ -0,0 +1,189 @@
+<script setup lang="ts">
+import { inject, ref } from 'vue';
+import { useSessionStore } from '@/stores/session';
+
+import feather from 'feather-icons';
+
+const sessionStore = useSessionStore();
+const apiBaseUrl = inject('apiBaseURL');
+
+const emit = defineEmits<{
+	(e: 'update'): void,        // Event fired when the announcement data is updated.
+	(e: 'delete'): void,        // Event fired when the announcement is deleted.
+}>();
+
+const props = defineProps<{
+	id: number,                 // ID of the announcement in the announcements table.
+	authorUsername: string,     // ID of the announcement's original author.
+	body: string,               // The body text of the announcement.
+	creationDate: string,       // The original creation date.
+}>();
+
+/* The locally-edited body text.
+ */
+const body = ref<string>(props.body);
+
+/* Is the announcement data currently being edited?
+ */
+const currentlyEditing = ref<boolean>(false);
+
+async function updateAnnouncement()
+{
+	await fetch(`${apiBaseUrl}/v1/announcement/${props.id}`,
+		{
+			method: 'PUT',
+			headers: {
+				'Accept': 'application/json',
+				'Authorization': `Bearer ${sessionStore.session}`,
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				'body': body.value.toString(),
+			}),
+		})
+		.then((response) =>
+		{
+			// TODO: make sure response is successful.
+			currentlyEditing.value = false;
+
+			emit('update');
+		});
+}
+
+async function deleteAnnouncement()
+{
+	await fetch(`${apiBaseUrl}/v1/announcement/${props.id}`,
+		{
+			method: 'DELETE',
+			headers: {
+				'Accept': 'application/json',
+				'Authorization': `Bearer ${sessionStore.session}`,
+			},
+		})
+		.then((response) =>
+		{
+			// TODO: make sure response is successful.
+			emit('delete');
+		});
+}
+
+function toggleEditState()
+{
+	currentlyEditing.value = !currentlyEditing.value;
+
+	if (!currentlyEditing.value)
+	{
+		body.value = props.body;
+	}
+}
+</script>
+
+<template>
+	<form>
+		<textarea
+			v-model="body"
+			maxlength="1000"
+			:disabled="!currentlyEditing"
+		></textarea>
+		<div class="character-count">
+			<span v-show="currentlyEditing">{{ body.length }}/1000</span>
+		</div>
+
+		<div class="button-container">
+			<button @click="toggleEditState" type="button">
+				<i v-if="currentlyEditing" v-html="feather.icons['arrow-left'].toSvg({ stroke: 'white' })"/>
+				<i v-else v-html="feather.icons['edit'].toSvg({ stroke: 'white' })"/>
+			</button>
+
+			<button v-if="!currentlyEditing" @click="deleteAnnouncement" type="button">
+				<i v-html="feather.icons['trash'].toSvg({ stroke: 'white' })"/>
+			</button>
+
+			<button v-else @click="updateAnnouncement" type="button">
+				<i v-html="feather.icons['upload'].toSvg({ stroke: 'white' })"/>
+			</button>
+		</div>
+
+		<hr>
+	</form>
+
+</template>
+
+<style scoped>
+form {
+	width:          90%;
+
+	display:        flex;
+	flex-direction: column;
+
+	padding-top:    0.85rem;
+}
+
+hr {
+	margin-top: 1rem;
+	height: 1px;
+	margin-bottom: 1rem;
+	opacity: 0.06;
+	width: 90%;
+	align-self: center;
+
+	border: none;
+	background-color: var(--col-text-dark);
+}
+
+textarea {
+	resize:           none;
+
+	padding:          0.5rem;
+	background-color: transparent;
+	color:            var(--col-text-dark);
+
+	border-style: solid;
+	border-width: thin;
+	border-color: currentColor;
+	border-radius: 5px;
+}
+
+textarea:disabled {
+	border-color: transparent;
+}
+
+.character-count {
+	height:  0.85rem;
+	opacity: 0.5;
+}
+
+.character-count > span {
+	font-size:   0.85rem;
+	font-family: monospace;
+}
+
+button {
+	display:          flex;
+	align-items:      center;
+	justify-content:  center;
+
+	aspect-ratio:     1;
+
+	background-color: transparent;
+
+	cursor:           pointer;
+
+	border-width: thin;
+	border-style: solid;
+	border-radius:    5px;
+
+	padding:          0.65rem;
+}
+
+.button-container {
+	align-self:      end;
+
+	display:         flex;
+	align-items:     center;
+	justify-content: center;
+	column-gap:      1rem;
+
+	padding-bottom:  0.85rem;
+}
+</style>
--- vue/src/views/admin/Announcements.vue
+++ vue/src/views/admin/Announcements.vue
@@ -1,11 +1,149 @@
 <script setup lang="ts">
+import AnnouncementForm from '@/components/admin/AnnouncementForm.vue';
+import { inject, ref } from 'vue';
+import { useSessionStore } from '@/stores/session';
 
+const apiBaseUrl = inject('apiBaseURL');
+const sessionStore = useSessionStore();
+
+const announcements = ref<[]>([]);
+
+const announcementBody = ref<string>('');
+
+async function fetchAnnouncements()
+{
+	await fetch(`${apiBaseUrl}/v1/announcements`,
+		{
+			method: 'GET',
+			headers: {
+				'Accept': 'application/json',
+			},
+		})
+		.then(async (response) =>
+		{
+			let json = await response.json();
+			announcements.value = json.data['announcements'];
+		});
+}
+
+async function postAnnouncement()
+{
+	await fetch(`${apiBaseUrl}/v1/announcements`,
+		{
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Authorization': `Bearer ${sessionStore.session}`,
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				body: announcementBody.value
+			}),
+		})
+		.then(async (response) =>
+		{
+			// TODO: maybe make sure it posted correctly, popup some sort of error if it didnt?
+			// TODO: client side validation.
+		});
+
+	announcementBody.value = '';
+	await fetchAnnouncements();
+}
+
+fetchAnnouncements();
 </script>
 
 <template>
-	Announcements
+	<h1>Manage Announcements</h1>
+	<form>
+		<textarea
+			v-model="announcementBody"
+			maxlength="1000"
+		></textarea>
+		<div class="character-count">
+			<span>{{ announcementBody.length }}/1000</span>
+		</div>
+
+		<input
+			:disabled="announcementBody.length === 0"
+			type="submit"
+			value="POST"
+			@click.prevent="postAnnouncement"
+		>
+	</form>
+
+	<AnnouncementForm
+		v-if="announcements.length > 0"
+		v-for="(announcement) in announcements"
+		:key="announcement['id']"
+
+		:id="announcement['id']"
+		:author-username="announcement['author']['username']"
+		:body="announcement['body']"
+		:creation-date="announcement['creationDate']"
+
+		@delete="fetchAnnouncements"
+	/>
+	<span v-else>No Posted Announcements</span>
 </template>
 
 <style scoped>
+	h1 {
+		align-self: start;
+		margin-left: 5%;
+		margin-bottom: 1rem;
+	}
 
+	li {
+		list-style-type: none;
+	}
+
+	span {
+		width: 100%;
+		text-align: center;
+
+		opacity: 0.5;
+	}
+
+	form {
+		display: flex;
+		flex-direction: column;
+		width: 90%;
+
+		margin-bottom: 2rem;
+	}
+
+	input[type="submit"] {
+		align-self: end;
+		padding-left: 1rem;
+		padding-right: 1rem;
+		padding-top: 0.5rem;
+		padding-bottom: 0.5rem;
+	}
+
+	textarea {
+		resize:           none;
+
+		padding:          0.5rem;
+		background-color: transparent;
+		color:            var(--col-text-dark);
+
+		border-style: solid;
+		border-width: thin;
+		border-color: currentColor;
+		border-radius: 5px;
+
+		height: 10rem;
+		width: 100%;
+	}
+
+	.character-count {
+		height:  0.85rem;
+		opacity: 0.5;
+	}
+
+	.character-count > span {
+		font-size:   0.85rem;
+		font-family: monospace;
+	}
 </style>
