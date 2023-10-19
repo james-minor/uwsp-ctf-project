9ceed0d1bb7d2d91fa38dc35f5739f0b02e115d6
James Minor <james.minor.business@gmail.com>
2023-10-19 13:58:38 -0500

    Added announcement.route.ts

---
+++ express/src/routes/announcement.route.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import * as announcementController from '../controllers/announcement.controller';
+import adminGuard from '../auth/admin.guard';
+
+module.exports = function (router: Router)
+{
+	router.get('/announcements', [announcementController.getAll]);
+
+	router.get('/announcement/:id(\\d+)', [announcementController.get]);
+
+	router.post('/announcements', [adminGuard, announcementController.create]);
+
+	router.put('/announcement/:id(\\d+)', [adminGuard, announcementController.update]);
+
+	router.delete('/announcement/:id(\\d+)', [adminGuard, announcementController.remove]);
+}
