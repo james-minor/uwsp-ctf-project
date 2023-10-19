9c29cf64a5e80459a7ce9687948e99677a9681ee
James Minor <james.minor.business@gmail.com>
2023-10-19 18:46:39 -0500

    Implemented fetchData function, to avoid excessive boilerplate

---
+++ vue/src/api/fetchData.ts
@@ -0,0 +1,59 @@
+import { apiBaseURL } from '@/APIBaseURL';
+import { useSessionStore } from '@/stores/session';
+
+/**
+ * Function to remove a lot of the redundant boilerplate code being written to fetch data from our backend API.
+ *
+ * @param endpoint The api endpoint being requested. The endpoint does not require a leading forward slash; however,
+ * if one is provided it will be parsed out within the function.
+ * @param method The HTTP method to send to the API.
+ * @param body The optional Request body string. If passed an object value, will Stringify the body into a JSON string.
+ * If passed a string, will skip stringify step, and pass the string directly to the Request body.
+ */
+export default async function fetchData(
+	endpoint: string,
+	method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+	body?: any
+)
+{
+	const sessionStore = useSessionStore();
+
+	/* Defining the HTTP Request headers.
+	 */
+	const requestHeaders: HeadersInit = {
+		'Accept': 'application/json',
+		'Authorization': `Bearer ${sessionStore.session}`,
+		'Content-Type': 'application/json',
+	}
+
+	/* Removing the leading forward-slash from the endpoint.
+	 */
+	let parsedEndpoint= endpoint;
+	if (endpoint.charAt(0) === '/')
+	{
+		parsedEndpoint = endpoint.substring(1);
+	}
+
+	/* Parsing the body, if it is not already a string.
+	 */
+	let parsedBody = body;
+	if (typeof body !== 'string')
+	{
+		parsedBody = JSON.stringify(body);
+	}
+
+	switch (method)
+	{
+		case 'GET':
+			return await fetch(`${apiBaseURL}/v1/${parsedEndpoint}`, {
+				method: method,
+				headers: requestHeaders,
+			});
+		default:
+			return await fetch(`${apiBaseURL}/v1/${parsedEndpoint}`, {
+				method: method,
+				headers: requestHeaders,
+				body: parsedBody ? parsedBody : '',
+			});
+	}
+}
