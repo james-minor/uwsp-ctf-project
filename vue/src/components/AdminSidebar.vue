51f5e391f9f9b846698f568b56325a6c35f8ffe5
James Minor <james.minor.business@gmail.com>
2023-10-20 21:37:36 -0500

    Added light mode support to AdminSidebar

--- vue/src/components/AdminSidebar.vue
+++ vue/src/components/AdminSidebar.vue
@@ -18,45 +18,60 @@
 </template>
 
 <style scoped>
+aside {
+	position:           fixed;
+	top:                var(--len-navbar-height);
+	left:               0;
+
+	width:              var(--len-sidebar-width);
+	height:             calc(100% - var(--len-navbar-height));
+
+	background-color:   var(--col-body-dark-100);
+	border-right-color: var(--col-body-dark-200);
+	border-right-width: 2px;
+	border-right-style: solid;
+}
+
+nav {
+	height:         100%;
+
+	display:        flex;
+	flex-direction: column;
+	align-items:    stretch;
+}
+
+a {
+	display:         flex;
+	align-items:     center;
+	justify-content: center;
+
+	color:           var(--col-text-dark);
+
+	flex:            1;
+
+	text-decoration: none;
+
+	outline:         none;
+	transition:      background-color 0.2s;
+}
+
+a:focus-visible,
+a:hover {
+	background-color: var(--col-body-dark-200);
+}
+
+@media (prefers-color-scheme: light) {
 	aside {
-		position: fixed;
-		top: var(--len-navbar-height);
-		left: 0;
-
-		width: var(--len-sidebar-width);
-		height: calc(100% - var(--len-navbar-height));
-
-		background-color: var(--col-body-dark-100);
-		border-right-color: var(--col-body-dark-200);
-		border-right-width: 2px;
-		border-right-style: solid;
-	}
-
-	nav {
-		height: 100%;
-
-		display: flex;
-		flex-direction: column;
-		align-items: stretch;
+		background-color: var(--col-body-light-100);
 	}
 
 	a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-
-		color: white;
-
-		flex: 1;
-
-		text-decoration: none;
-
-		outline: none;
-		transition: background-color 0.2s;
+		color: var(--col-text-light);
 	}
 
 	a:focus-visible,
 	a:hover {
-		background-color: var(--col-body-dark-200);
+		background-color: var(--col-body-light-200);
 	}
+}
 </style>
