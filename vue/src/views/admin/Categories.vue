52f13d20f3e53670fac6e83d43f62236436abb94
James Minor <james.minor.business@gmail.com>
2023-10-21 23:19:36 -0500

    Implemented Categories admin view functionality

--- vue/src/views/admin/Categories.vue
+++ vue/src/views/admin/Categories.vue
@@ -1,11 +1,82 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import fetchData from '@/api/fetchData';
+import CategoryEditor from '@/components/admin/CategoryEditor.vue';
+import AppButton from '@/components/buttons/AppButton.vue';
+
+const categories = ref<[]>([]);
+const newCategoryTitle = ref<string>('');
+
+async function fetchCategories()
+{
+	await fetchData('categories', 'GET')
+		.then(async (response) =>
+		{
+			let json = await response.json();
+			categories.value = json.data['categories'];
+		});
+}
+fetchCategories();
+
+async function postCategory()
+{
+	await fetchData('categories', 'POST', { 'title': newCategoryTitle.value.toString() })
+		.then(async (response) =>
+		{
+			// TODO: maybe make sure it posted correctly, popup some sort of error if it didnt?
+		});
+
+	newCategoryTitle.value = '';
+	await fetchCategories();
+}
 
 </script>
 
 <template>
-	Categories
+	<h1>Manage Categories</h1>
+
+	<form>
+		<input type="text" maxlength="15" name="title" v-model="newCategoryTitle">
+
+		<AppButton :disabled="newCategoryTitle.length === 0" @click="postCategory">POST</AppButton>
+	</form>
+
+	<CategoryEditor
+		v-if="categories.length > 0"
+		v-for="(category) in categories"
+		:key="category['id']"
+
+		:id="category['id']"
+		:title="category['title']"
+
+		@refresh="fetchCategories()"
+	/>
+	<span class="empty-list" v-else>No Posted Announcements</span>
 </template>
 
 <style scoped>
+h1 {
+	align-self:    start;
+	margin-left:   5%;
+	margin-bottom: 1rem;
+}
 
+form {
+	width:         90%;
+
+	margin:        1rem 5%;
+
+	border:        thin solid white;
+	border-radius: 5px;
+
+	display:       flex;
+	align-items:   center;
+
+	padding:       1rem;
+	column-gap:    1rem;
+}
+
+input {
+	flex: 1;
+}
 </style>
