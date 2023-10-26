68ef111e17a616f903957b1675a695e6a00d112b
James Minor <james.minor.business@gmail.com>
2023-10-26 09:59:20 -0500

    Implemented getAll()

--- vue/src/views/admin/Waves.vue
+++ vue/src/views/admin/Waves.vue
@@ -1,10 +1,46 @@
 <script setup lang="ts">
-
+import { ref } from 'vue';
+import fetchData from '@/api/fetchData';
 import ViewHeader from '@/components/admin/ViewHeader.vue';
+import EmptyListFooter from '@/components/admin/EmptyListFooter.vue';
+import ModelEditor from '@/components/admin/ModelEditor.vue';
+
+const waves = ref<[]>([]);
+
+async function fetchWaves()
+{
+	await fetchData('waves', 'GET')
+		.then(async (response) =>
+		{
+			let json = await response.json();
+			waves.value = json.data['waves'];
+		});
+}
+
+fetchWaves();
 </script>
 
 <template>
 	<ViewHeader>Manage Release Waves</ViewHeader>
+
+	<ModelEditor
+		v-if="waves.length > 0"
+		v-for="wave in waves"
+		model="wave"
+
+		:id="wave['id']"
+		:fields="[
+			{
+				name: 'releaseDate',
+				type: 'text',
+				editable: true,
+				initialValue: wave['releaseDate'],
+				modelValue: wave['releaseDate'],
+			},
+		]"
+	/>
+
+	<EmptyListFooter v-else>No Posted Waves</EmptyListFooter>
 </template>
 
 <style scoped>
