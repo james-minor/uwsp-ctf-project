33a6c51f7d221f2c386a3634232530a7ddc00d68
James Minor <james.minor.business@gmail.com>
2023-10-26 09:04:09 -0500

    Implemented Teams view functionality

--- vue/src/views/admin/Teams.vue
+++ vue/src/views/admin/Teams.vue
@@ -1,9 +1,42 @@
 <script setup lang="ts">
+import fetchData from '@/api/fetchData';
+import { ref } from 'vue';
+import ModelEditor from '@/components/admin/ModelEditor.vue';
+import ViewHeader from '@/components/admin/ViewHeader.vue';
 
+const teams = ref<[]>([]);
+
+async function fetchTeams()
+{
+	await fetchData('teams', 'GET')
+		.then(async (response) =>
+		{
+			let json = await response.json();
+			teams.value = json.data['teams'];
+		});
+}
+
+fetchTeams();
 </script>
 
 <template>
-	Teams
+	<ViewHeader>Manage Teams</ViewHeader>
+
+	<ModelEditor
+		v-for="team in teams"
+		model="team"
+
+		:id="team['id']"
+		:fields="[
+			{
+				name: 'name',
+				type: 'text',
+				editable: false,
+				initialValue: team['name'],
+				modelValue: team['name'],
+			},
+		]"
+	/>
 </template>
 
 <style scoped>
