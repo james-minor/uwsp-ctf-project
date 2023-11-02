3483f6ad58d035bb18324d4d8c542866c0a8e827
James Minor <james.minor.business@gmail.com>
2023-11-02 11:38:33 -0500

    Added route to delete team.

--- express/src/routes/team.route.ts
+++ express/src/routes/team.route.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import * as teamController from '../controllers/team.controller';
+import { adminGuard } from '../auth/guard';
 
 module.exports = function (router: Router)
 {
 	router.get('/teams', [teamController.getAll]);
+
+	router.delete('/team/:id(\\d+)', [adminGuard, teamController.remove])
 }
