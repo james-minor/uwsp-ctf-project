40aa11955e69029a0e03beef46ae1cdc6ae53b8a
James Minor <james.minor.business@gmail.com>
2023-11-02 11:39:47 -0500

    Added remove(). Fixed issues with create()

--- express/src/controllers/team.controller.ts
+++ express/src/controllers/team.controller.ts
@@ -3,10 +3,17 @@ import { APIResponse } from '../interfaces/APIResponse';
 import client from '../Client';
 import { teamNameTaken, validInviteCodeLength, validTeamNameLength } from '../utils/team';
 import crypto from 'node:crypto';
+import { Team } from '@prisma/client';
+import bcrypt from 'bcryptjs';
 
 export async function getAll(req: Request, res: Response<APIResponse>)
 {
-	await client.team.findMany({})
+	await client.team.findMany({
+		select: {
+			id: true,
+			name: true,
+		}
+	})
 		.then((teams) =>
 		{
 			res.status(200).json({
@@ -20,6 +27,8 @@ export async function getAll(req: Request, res: Response<APIResponse>)
 
 export async function create(req: Request, res: Response<APIResponse>, next: NextFunction)
 {
+	let team: Team | undefined = undefined;
+
 	let errors: {
 		teamName: string | undefined,
 		inviteCode: string | undefined,
@@ -37,18 +46,73 @@ export async function create(req: Request, res: Response<APIResponse>, next: Nex
 
 	/* Validating the invite code, if applicable.
 	 */
+	if (req.body['createNewTeam'] === true && await teamNameTaken(req.body.teamName))
+	{
+		errors.teamName = 'Team name is taken.'
+	}
+
+	if (await teamNameTaken(req.body.teamName) && !validInviteCodeLength(req.body.inviteCode))
+	{
+		errors.inviteCode = 'Invite code must be 8 characters.';
+	}
+
+	/* Attempting to authenticate an existing team using the invite code.
+	 */
 	if (await teamNameTaken(req.body.teamName))
 	{
-		if (!validInviteCodeLength(req.body.inviteCode))
+		await client.team.findUnique({
+			where: {
+				name: req.body.teamName,
+				inviteCode: req.body.inviteCode,
+			}
+		}).then((result) =>
 		{
-			errors.inviteCode = 'Invite code must be 8 characters.';
-		}
+			if(result)
+			{
+				team = result;
+			}
+			else
+			{
+				errors.inviteCode = 'Incorrect invite code.';
+			}
+		});
+	}
+	/* Attempting to create a new team.
+	 */
+	else
+	{
+		/* Generating the random invite code.
+		 */
+		const inviteCode = crypto.randomBytes(4).toString('hex');
+
+		/* Creating the new team.
+		 */
+		team = await client.team.create({
+			data: {
+				name: req.body.teamName,
+				inviteCode: inviteCode,
+			}
+		}).then((result) =>
+		{
+			if (result)
+			{
+				return result;
+			}
+		});
 	}
 
 	/* Sending back errors, if any.
 	 */
 	if (errors.teamName || errors.inviteCode)
 	{
+		/* A new user entry has already been created, so we need to delete that before returning data to the client.
+		 */
+		await client.user.delete({
+			where: {
+				username: req.body.username,
+			}
+		});
+
 		res.status(400).json({
 			success: false,
 			errors: [
@@ -65,85 +129,53 @@ export async function create(req: Request, res: Response<APIResponse>, next: Nex
 		return;
 	}
 
-	/* Creating the team, if needed.
+	/* Adding the user to the team.
 	 */
-	// TODO: clean up this branching mess
-	if (!await teamNameTaken(req.body.teamName))
+	await client.user.findUnique({
+		where: {
+			username: req.body.username
+		}
+	}).then((user) =>
 	{
-		/* Generating the random invite code.
-		 */
-		const inviteCode = crypto.randomBytes(4).toString('hex');
-
-		/* Creating the new team.
-		 */
-		await client.team.create({
-			data: {
-				name: req.body.teamName,
-				inviteCode: inviteCode,
-			}
-		}).then(async (team) =>
+		return !!(user && bcrypt.compare(req.body.password, user.passwordHash));
+	}).then(async (authenticated) =>
+	{
+		if (authenticated && team)
 		{
-			/* Adding the user to the newly created team.
-			 */
 			await client.user.update({
 				where: {
-					username: req.body.username
+					username: req.body.username,
 				},
 				data: {
 					teamId: team.id,
 				}
-			}).then(() =>
-			{
-				next();
 			});
-		}).catch(() =>
-		{
-			res.status(500).json({
-				success: false,
-			});
-		});
-	}
-	else
+
+			next();
+		}
+	}).catch(() =>
 	{
-		/* Validating the user entered the correct invite code.
-		 */
-		await client.team.findUnique({
-			where: {
-				name: req.body.teamName,
-				inviteCode: req.body.inviteCode,
-			}
-		}).then(async (team) =>
-		{
-			if (team)
-			{
-				/* Adding the user to the existing team.
-				 */
-				await client.user.update({
-					where: {
-						username: req.body.username
-					},
-					data: {
-						teamId: team.id,
-					}
-				}).then(() =>
-				{
-					next();
-				});
-			}
-			else
-			{
-				res.status(400).json({
-					success: false,
-					errors: [
-						{ key: 'inviteCode', message: 'Incorrect invite code.' }
-					]
-				});
-			}
-		}).catch(() =>
-		{
-			res.status(500).json({
-				success: false,
-			});
+		res.status(500).json({
+			success: false,
 		});
-	}
+	});
+}
+
+export async function remove(req: Request, res: Response<APIResponse>, next: NextFunction)
+{
+	await client.team.delete({
+		where: {
+			id: parseInt(req.params['id']),
+		},
+	}).then(() =>
+	{
+		res.status(200).json({
+			success: true,
+		});
+	}).catch(() =>
+	{
+		res.status(500).json({
+			success: false,
+		});
+	})
 }
