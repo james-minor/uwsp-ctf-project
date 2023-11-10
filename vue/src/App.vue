59a377a24a7a395ca4835d03bf0f267c499a5930
James Minor <james.minor.business@gmail.com>
2023-11-10 16:38:23 -0600

    Added app-wrapper div to allow for max screen height body

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,44 +1,59 @@
 <script setup lang="ts">
-	import { provide, ref } from 'vue';
-	import { apiBaseURL } from '@/APIBaseURL';
-	import NavigationBar from '@/components/NavigationBar.vue';
-	import AdminSidebar from '@/components/AdminSidebar.vue';
-	import router from '@/router/router';
+import { provide, ref } from 'vue';
+import { apiBaseURL } from '@/APIBaseURL';
+import NavigationBar from '@/components/NavigationBar.vue';
+import AdminSidebar from '@/components/AdminSidebar.vue';
+import router from '@/router/router';
 
-	const navbarVisible = ref<boolean>(true);
-	const sidebarVisible = ref<boolean>(false);
+const navbarVisible = ref<boolean>(true);
+const sidebarVisible = ref<boolean>(false);
 
-	router.beforeEach((to, from) =>
-	{
-		navbarVisible.value = to.meta.hideMainNav ? !to.meta.hideMainNav : true;
-		sidebarVisible.value = to.meta.showAdminNav ? to.meta.showAdminNav : false;
-	})
+router.beforeEach((to, from) =>
+{
+	navbarVisible.value = to.meta.hideMainNav ? !to.meta.hideMainNav : true;
+	sidebarVisible.value = to.meta.showAdminNav ? to.meta.showAdminNav : false;
+})
 
-	provide<string>('apiBaseURL', apiBaseURL);
+provide<string>('apiBaseURL', apiBaseURL);
 </script>
 
 <template>
-	<admin-sidebar v-if="sidebarVisible"></admin-sidebar>
-	<navigation-bar v-if="navbarVisible"></navigation-bar>
+	<div class="app-wrapper">
+		<admin-sidebar v-if="sidebarVisible"></admin-sidebar>
+		<navigation-bar v-if="navbarVisible"></navigation-bar>
 
-	<main :class="{ 'sidebarPresent' : sidebarVisible, 'navbarPresent' : navbarVisible }">
-		<router-view></router-view>
-	</main>
+		<main :class="{ 'sidebarPresent' : sidebarVisible, 'navbarPresent' : navbarVisible }">
+			<router-view></router-view>
+		</main>
+	</div>
 </template>
 
 <style scoped>
-	main {
-		display: flex;
-		flex-direction: column;
+.app-wrapper {
+	position:       absolute;
+	top:            0;
+	left:           0;
 
-		align-items: center;
-	}
+	min-height:     100svh;
+	width:          100%;
 
-	.navbarPresent {
-		margin-top: var(--len-navbar-height);
-	}
+	display:        flex;
+	flex-direction: column;
+}
 
-	.sidebarPresent {
-		margin-left: var(--len-sidebar-width);
-	}
+main {
+	flex:           1;
+	display:        flex;
+	flex-direction: column;
+
+	align-items:    center;
+}
+
+.navbarPresent {
+	margin-top: var(--len-navbar-height);
+}
+
+.sidebarPresent {
+	margin-left: var(--len-sidebar-width);
+}
 </style>
