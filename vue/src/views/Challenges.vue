6df83b5db5c28e8aff806b276f9afc025fd5e283
James Minor <james.minor.business@gmail.com>
2023-11-13 23:25:28 -0600

    Implemented the Challenges view

--- vue/src/views/Challenges.vue
+++ vue/src/views/Challenges.vue
@@ -1,11 +1,168 @@
-<script setup lang="ts">   
+<script setup lang="ts">
+
+import fetchData from '@/api/fetchData';
+
+import * as feather from 'feather-icons'
+
+import { ref } from 'vue';
+import fetchModelArray from '@/api/fetchModelArray';
+import ChallengeDropdown from '@/components/ChallengeDropdown.vue';
+import AppButton from '@/components/buttons/AppButton.vue';
+import AttachmentButton from '@/components/buttons/AttachmentButton.vue';
+import FormInput from '@/components/form/FormInput.vue';
+import FormError from '@/components/form/FormError.vue';
+import { useSessionStore } from '@/stores/session';
+
+const sessionStore = useSessionStore();
+
+const flagData = ref<{
+	value: string,
+	error: string | undefined,
+}>({
+	value: '',
+	error: undefined,
+});
+
+const categories = ref<[]>([]);
+const challenges = ref<[]>([]);
+const selected = ref<any>(undefined);
+
+fetchModelArray('categories').then((res) => categories.value = res);
+fetchModelArray('challenges').then((res) => challenges.value = res);
+
+function getChallengeInCategory(categoryId: number): object[]
+{
+	let output: any[] = [];
+	for (let challenge of challenges.value)
+	{
+		if (challenge['category']['id'] === categoryId)
+		{
+			output.push(challenge);
+		}
+	}
+
+	return output;
+}
+
+function onSelectChallenge(challengeId: number)
+{
+	for (let challenge of challenges.value)
+	{
+		if (challenge['id'] === challengeId)
+		{
+			selected.value = challenge;
+		}
+	}
+}
+
+async function attemptSolve()
+{
+	await fetchData(`challenge/${selected.value['id']}/solve`, 'POST', { flag: flagData.value.value })
+		.then(async (res) =>
+		{
+			const json = await res.json();
+
+			if (!json['errors'])
+			{
+				return;
+			}
+
+			flagData.value.error = json['errors'].filter((error: any) =>
+			{
+				return error.key === 'flag';
+			})[0].message;
+		});
+}
+
 
 </script>
 
 <template>
-    <h1>Challenges page</h1>
+	<div class="challenge-view">
+		<div :class="{'challenge-list': true, 'challenge-list--empty': challenges.length === 0}">
+			<ChallengeDropdown
+				v-for="category in categories"
+				v-if="categories.length > 0"
+
+				:category-title="category['title']"
+				:challenges="getChallengeInCategory(category['id'])"
+
+				@select="onSelectChallenge"
+			/>
+			<span v-else>No Challenges are Released</span>
+		</div>
+		<div class="challenge-preview">
+			<div class="challenge-preview__content" v-if="selected">
+				<h2>{{ selected['title'] }}</h2>
+				<p>{{ selected['body'] }}</p>
+
+				<AttachmentButton
+					v-for="attachment in selected['attachments']"
+					:attachment-id="attachment['id']"
+				>{{ attachment['title'] }}
+				</AttachmentButton>
+
+				<FormInput
+					v-if="sessionStore.session"
+
+					v-model="flagData.value"
+					name="flag"
+					type="text"
+
+					:icon="feather.icons['flag']"
+				/>
+				<FormError :error="flagData.error"></FormError>
+				<AppButton
+					v-if="!sessionStore.hasElevatedPrivileges && sessionStore.session"
+					@click="attemptSolve"
+				>
+					Solve
+				</AppButton>
+			</div>
+			<span v-else>Select a Challenge</span>
+		</div>
+	</div>
 </template>
 
-<style scoped> 
+<style scoped>
+.challenge-view {
+	width:                 100%;
+	flex:                  1;
 
+	display:               grid;
+	grid-template-columns: repeat(2, 1fr);
+}
+
+.challenge-list {
+	width:           100%;
+
+	border-right:    solid 2px var(--col-body-dark-200);
+}
+
+.challenge-list--empty {
+	display:         flex;
+	flex-direction:  column;
+
+	align-items:     center;
+	justify-content: center;
+}
+
+.challenge-preview {
+	width:           100%;
+	height:          100%;
+
+	display:         flex;
+	align-items:     center;
+	justify-content: center;
+}
+
+.challenge-preview__content {
+	margin:          1rem min(10%, 6rem);
+	width:           100%;
+
+	display:         flex;
+	flex-direction:  column;
+	justify-content: center;
+	row-gap:         1rem;
+}
 </style>
